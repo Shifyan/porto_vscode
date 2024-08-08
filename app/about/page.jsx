9027f3a2e6ddc9f2481a5373a15f339d4d798a77
Shifyannn <asifyan01@gmail.com>
2024-08-08 12:12:38 +0700

    finish

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,13 @@
 const AboutPage = () => {
   return (
     <>
-      <h3>A Little Bit About Me</h3>
+      <h3>
+        Saya adalah Web Developer dengan pengalaman dalam HTML, CSS, dan
+        JavaScript. Saya juga mahir menggunakan framework seperti React dan
+        Next.js, serta pustaka gaya seperti Tailwind CSS dan Bootstrap. Di sisi
+        backend, saya berpengalaman dengan Node.js dan PHP. Saya fokus pada
+        pengembangan web yang efisien dan efektif.
+      </h3>
     </>
   );
 };
