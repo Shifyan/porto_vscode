2f48a89a3ba0e467ab55a84d2283eefa1e5126a2
Shifyannn <asifyan01@gmail.com>
2024-08-08 12:28:17 +0700

    fix errors

--- app/api/projects.js
+++ app/api/projects.js
@@ -1,10 +1,12 @@
-import data from './projects.json';
+import data from "./projects.json";
 
 export const getProjects = () => {
   return data;
 };
 
-export default (req, res) => {
+const getMyProject = (req, res) => {
   const projects = getProjects();
   res.json(projects);
 };
+
+export default getMyProject;
